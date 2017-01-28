e34104c61d903777328d80201c98a5b0ef59b878
Austin Brown <atb.austin@gmail.com>
2017-01-27 21:51:43 -0600

    Refactored PlayerController code and ball grows on Pick Up trigger.

--- UnityTutorial_1/Assets/Scripts/PlayerController.cs
+++ UnityTutorial_1/Assets/Scripts/PlayerController.cs
@@ -7,20 +7,30 @@ public class PlayerController : MonoBehaviour {
 
 	// Control
 	public float speed;
+	private string horiz = "Horizontal";
+	private string vert = "Vertical";
 
 	// Physics
 	private Rigidbody rb;
 
-	// Count of number of pick ups
+	// Pick Ups
 	private int count;
+	public int maxPickUps;
 	public Text countText;
 	public Text winText;
+	public List<string> pickUpTags;
+
+	// Non-Control Transformations
+	public float scaleFactor;
 
 	void Start ()
 	{
+		// Rigid body
 		rb = GetComponent<Rigidbody> ();
+
+		// Pick Ups
 		count = 0;
-		SetCountText();
+		SetCountText(0);
 		winText.enabled = false;
 	}
 
@@ -34,30 +44,51 @@ public class PlayerController : MonoBehaviour {
 	// Moving a rigidbody is a physics update
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis (horiz);
+		float moveVertical = Input.GetAxis (vert);
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
 
 		rb.AddForce (movement * speed);
 	}
 
-	void OnTriggerEnter(Collider other) {
-		// Destroy(other.gameObject);
-		if (other.gameObject.CompareTag ("Pick Up")) 
+	void OnTriggerEnter(Collider other) 
+	{
+		if (pickUpTags.Contains (other.gameObject.tag))
 		{
+			// Make Pick Up disappear
 			other.gameObject.SetActive (false);
-			count++;
-			SetCountText();
+
+			CollectPickUp ();
+		}
+	}
+	
+	void SetCountText(int count)
+	{
+		countText.text = "Count: " + count.ToString();
+	}
+
+	void ToggleWinText()
+	{
+		winText.enabled = !winText.enabled;
+	}
+		
+	void CollectPickUp()
+	{
+		// Rigid body
+		Grow();
+
+		// Score counting
+		count++;
+		SetCountText (count);
+		if (count >= maxPickUps) 
+		{
+			ToggleWinText ();
 		}
 	}
 
-	void SetCountText()
+	void Grow()
 	{
-		countText.text = "Count: " + count.ToString ();
-		if (count >= 12) 
-		{
-			winText.enabled = true;
-		}
+		rb.transform.localScale += new Vector3(scaleFactor, scaleFactor, scaleFactor);
 	}
 }
